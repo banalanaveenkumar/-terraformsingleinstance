fab6acdc9cb7f653c4888d36abeb7f873f99b229
banalanaveenkumar <136616395+banalanaveenkumar@users.noreply.github.com>
2024-03-21 11:37:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,10 +4,10 @@ variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
     default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+      #  us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+	#	us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+	#	us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+	#	us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
     }
 }
 variable "vpc_cidr" {}
@@ -28,7 +28,7 @@ variable "azs" {
   type = "list"
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-variable "environment" { default = "dev" }
+variable "environment" { default = "test" }
 variable "instance_type" {
   type = "map"
   default = {
